18d64d3a81ea4843f05dd0418ee8f3d71b83aebe
Silas Vedder <silas@silasvedder.xyz>
2023-04-21 13:21:01 +0200

    Added API project

---
+++ MicroVerseAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MicroVerseAPI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
