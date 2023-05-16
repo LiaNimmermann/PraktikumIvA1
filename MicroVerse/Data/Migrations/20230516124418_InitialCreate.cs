6b6c95032e1b011d8413f58ae5799772223c76bc
Alex37t <mansy337@gmail.com>
2023-05-16 14:48:38 +0200

    Regenerated migration.

--- MicroVerse/Data/Migrations/20230516124418_InitialCreate.cs
+++ MicroVerse/Data/Migrations/20230516124418_InitialCreate.cs
@@ -36,6 +36,7 @@ namespace MicroVerse.Data.Migrations
                     Body = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     AuthorEmail = table.Column<string>(type: "nvarchar(450)", nullable: false),
                     ReactsToId = table.Column<int>(type: "int", nullable: true),
+                    Activation = table.Column<int>(type: "int", nullable: false),
                     CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
                 },
                 constraints: table =>
@@ -54,6 +55,25 @@ namespace MicroVerse.Data.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
+            migrationBuilder.CreateTable(
+                name: "Vote",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Upvote = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vote", x => new { x.PostId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_Vote_Post_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Post",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
             migrationBuilder.CreateIndex(
                 name: "IX_Post_AuthorEmail",
                 table: "Post",
@@ -67,6 +87,9 @@ namespace MicroVerse.Data.Migrations
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "Vote");
+
             migrationBuilder.DropTable(
                 name: "Post");
 
