8334ad76cb6d28fa5735dc6b4779ec951d572f1d
Lisa Otterbach <lisaotterbach2001@gmail.com>
2023-06-20 14:18:51 +0200

    PostSender

---
+++ MicroVerseMaui/Models/PostSender.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MicroVerseMaui.Models
+{
+    public class PostSender : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            var text = (string)parameter;
+
+        }
+    }
+}
