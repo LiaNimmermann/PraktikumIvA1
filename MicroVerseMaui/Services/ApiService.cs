6240aeeac93e8407db7fbbd0fde5e6f4e279926e
Alex37t <Mansy337@gmail.com>
2023-06-24 17:59:24 +0200

    Allow Android https connection over localhost

--- MicroVerseMaui/Services/ApiService.cs
+++ MicroVerseMaui/Services/ApiService.cs
@@ -1,32 +1,41 @@
 ﻿using MicroVerseMaui.Models;
+using System;
 using System.Net.Http.Json;
-using static System.Net.WebRequestMethods;
-
 
 namespace MicroVerseMaui.Services
 {
     // Get Api data from local host
     public class ApiService
     {
-        HttpClient httpClient;
-
         public ApiService()
         {
-            httpClient = new HttpClient();
         }
         List<Post> postsList = new();
         //Get list of all posts
         public async Task<List<Post>> GetPosts()
         {
-            var url = "https://localhost:7028/api/Post";
-            var response = await httpClient.GetAsync(url);
+            HttpResponseMessage response;
+
+            // If android, use helper for https request
+            if (DeviceInfo.Platform == DevicePlatform.Android)
+            {
+                var devSslHelper = new DevHttpsConnectionHelper(sslPort: 7028);
+                var http = devSslHelper.HttpClient;
+                // DevServerRootUrl = Localhost
+                response = await http.GetAsync(devSslHelper.DevServerRootUrl + "/api/Post/");
+            }
+            else // Windows
+            {
+                var httpClient = new HttpClient();
+                var url = "https://localhost:7028/api/Post";
+                response = await httpClient.GetAsync(url);
+            }
+
             if (response.IsSuccessStatusCode)
             {
                 postsList = await response.Content.ReadFromJsonAsync<List<Post>>();
             }
-            return postsList
-                  .OrderByDescending(post => post.CreatedAt).ToList();
-;
+            return postsList.OrderByDescending(post => post.CreatedAt).ToList();
         }
 
     }
