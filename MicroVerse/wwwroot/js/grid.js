ddee4a8040f0dba7b4542e3f1f407722ddf6f4c4
Lia Nimmermann <94193973+LiaNimmermann@users.noreply.github.com>
2023-07-12 11:34:50 +0200

    Statistics modification

--- MicroVerse/wwwroot/js/grid.js
+++ MicroVerse/wwwroot/js/grid.js
@@ -4,6 +4,7 @@
 // Write your JavaScript code.
 
 
+
 const mockData = {
     userPosting : {
         labels: ["User1", "User2", "User3", "User4", "User5"],
@@ -17,20 +18,132 @@ const mockData = {
         following: [1, 14, 3, 12, 7]
     },
 
-    mostUpvotedPost : {
+    mostUpvotedPost : {     //All votes accumulated to users aka Users with most upvotes
         labels: ["User1", "User2", "User3", "User4", "User5"],
-        content: ["Postcontent1", "postcontent2", "postcontent3", "postcontent4", "postcontent5"],
         upvotes: [50, 45, 30, 18, 10],
         downvotes: [1, 14, 3, 12, 7]
     },
 
-    mostDownvotedPost : {
+    mostDownvotedPost: {   //All votes accumulated to users aka Users with most downvotes
         labels: ["User1", "User2", "User3", "User4", "User5"],
-        content: ["Postcontent1", "postcontent2", "postcontent3", "postcontent4", "postcontent5"],
         upvotes: [1, 14, 3, 12, 7],
         downvotes: [50, 45, 30, 18, 10]
     }
 }
+
+var userPostingGrid;
+var mostFollowedUserGrid;
+var mostUpvotedPostsGrid;
+var mostDownvotedPostsGrid;
+
+function initUserPosting(data) {
+    var userPostingData = {
+        labels: data.labels,
+        datasets: [
+            {
+                label: "Posts all time",
+                data: data.postsAllTime
+            },
+            {
+                label: "Posts in the last 7 days",
+                data: data.postsLastWeek
+            },
+            {
+                label: "Posts today",
+                data: data.postsToday
+            }
+        ]
+    };
+    userPostingGrid = new Chart("userPosting", {
+        type: "bar",
+        data: userPostingData,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    })
+}
+
+function initMostFollowedUser(data) {
+    var mostFollowedUserData = {
+        labels: data.labels,
+        datasets: [
+            {
+                axis: 'y',
+                label: "Followers",
+                data: data.followers
+            },
+            {
+                axis: 'y',
+                label: "Following",
+                data: data.following
+            }]
+    };
+    mostFollowedUserGrid = new Chart("mostFollowedUsers", {
+        type: "bar",
+        data: mostFollowedUserData,
+        options: {
+            indexAxis: 'y',
+        }
+    })
+}
+
+function initMostUpvotedPosts(data) {
+    var mostUpvotedPostsData = {
+        labels: data.labels,
+        datasets: [
+            {
+                axis: 'y',
+                label: "Upvotes",
+                data: data.upvotes
+            },
+            {
+                axis: 'y',
+                label: "Downvotes",
+                data: data.downvotes
+            }]
+    };
+
+    mostUpvotedPostsGrid = new Chart("mostUpvotedPosts", {
+        type: "bar",
+        data: mostUpvotedPostsData,
+        options: {
+            indexAxis: 'y',
+        }
+    })
+}
+function initMostDownvotedPosts(data) {
+    var mostDownvotedPostsData = {
+        labels: data.labels,
+        datasets: [
+            {
+                axis: 'y',
+                label: "Downvotes",
+                data: data.downvotes
+            },
+            {
+                axis: 'y',
+                label: "Upvotes",
+                data: data.upvotes
+            }]
+    };
+
+    mostDownvotedPostsGrid = new Chart("mostDownvotedPosts", {
+        type: "bar",
+        data: mostDownvotedPostsData,
+        options: {
+            indexAxis: 'y',
+        }
+    })
+}
+
+initUserPosting(mockData.userPosting);
+initMostFollowedUser(mockData.mostFollowedUsers);
+initMostUpvotedPosts(mockData.mostUpvotedPost);
+initMostDownvotedPosts(mockData.mostDownvotedPost);
 //function getPostsLastWeek(array, i,) {
 //    return array[i].postsLastWeek
 //}
@@ -50,76 +163,3 @@ const mockData = {
 //    }
 //    return values;
 //}
-
-const userPostingData = {
-    labels: ["User1", "User2", "User3", "User4", "User5"],
-    datasets: [
-        {
-            label: "Posts all time",
-            data: [5, 24, 6, 22, 8]
-        },
-        {
-            label: "Posts in the last 7 days",
-            data: [1, 14, 3, 12, 7]
-        }]
-};
-
-const userPostingGrid = new Chart("userPosting", {
-    type: "bar",
-    data: userPostingData,
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-})
-
-
-const mostFollowedUserData = {
-    labels: ["User1", "User2", "User3", "User4", "User5"],
-    datasets: [
-        {
-            axis: 'y',
-            label: "Followers",
-            data: [50,45,30,18,10]
-        },
-        {
-            axis:'y',
-            label: "Following",
-            data: [1, 14, 3, 12, 7]
-        }]
-};
-
-const mostFollowedUserGrid = new Chart("mostFollowedUsers", {
-    type: "bar",
-    data: mostFollowedUserData,
-    options: {
-        indexAxis: 'y',
-    }
-})
-
-
-const mostUpvotedPostData = {
-    labels: ["User1", "User2", "User3", "User4", "User5"],
-    datasets: [
-        {
-            axis: 'y',
-            label: "Followers",
-            data: [50, 45, 30, 18, 10]
-        },
-        {
-            axis: 'y',
-            label: "Following",
-            data: [1, 14, 3, 12, 7]
-        }]
-};
-
-const mostUpvotedPostGrid = new Chart("mostUpvotedPosts", {
-    type: "bar",
-    data: mostUpvotedPostData,
-    options: {
-        indexAxis: 'y',
-    }
-})
